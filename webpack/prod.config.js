dede18cb327b3019da112b37e445fdd5ce84015d
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-07 20:46:27 -0300

    M2#A75 - Atualização do Webpack - atualizar storybook e remover erros

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -27,9 +27,6 @@ module.exports = {
       sourceMap: true
     }),
 
-    // Não deixa duplicar arquivos chamados novamente
-    new webpack.optimize.DedupePlugin(),
-
     // Ordena para carregar os mais leves primeiro
     new webpack.optimize.OccurrenceOrderPlugin(),
 
@@ -37,11 +34,12 @@ module.exports = {
   ],
 
   module: {
-    roles: [
+
+    rules: [
       common.standardPreLoader,
       common.jsLoader,
       Object.assign({}, common.cssLoader, {
-        use: ExtractTextPlugin.extract.apply({
+        use: ExtractTextPlugin.extract({
           fallback: common.cssLoader.use[0],
           use: common.cssLoader.use.slice(1)
         })
@@ -51,17 +49,4 @@ module.exports = {
 
   resolve: common.resolve
 }
-
-/*
-entry
-    Ponto de entrada que indica qual módulo o webpack deve usar para iniciar a construção do gráfico interno de dependência.
-    Ao definir um ponto de entrada, o webpack irá encontrar todas a dependências e fazer a importação.
-    Por padrão o ponto de entrada é definido no arquivo ./src/index.js, mas é possível definir um arquivo diferente ou até
-    mesmo múltiplos pontos de entrada no arquivo de configuração webpack.config.js.
-
-Output
-    A propriedade Output define o nome e local do pacote gerado pelo webpack.
-    O diretório padrão é o ./dist e o arquivo ./dist/main.js.
-    Para configurar, devemos definir um objeto output com as propriedades path e filename no arquivo de configuração do webpack:
-
-*/
+// webpack-dashboard --
